f091bc25339e88f8fdd141fe4534b5b0920829df
ariefzainuri <ariefzainuri@kotalogue.com>
2019-07-27 08:24:10 +0700

    Initial Commit

---
+++ Angkasa Pura Solusi/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Angkasa Pura Solusi
+//
+//  Created by Arief Zainuri on 27/07/19.
+//  Copyright © 2019 Gama Techno. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
